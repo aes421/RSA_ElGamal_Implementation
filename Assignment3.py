5e5e3f89f4e06ba4ffb866e59753ec4d1b6f95b2
Ashley Stallings <aesdev421@gmail.com>
2016-02-22 15:37:50 -0600

    El Gamal Key Generation and Encryption now implemented
    El Gamal Key Generation and Encryption now implemented.  Some values are
    hardcoded for testing purposes.  Decryption does not work.  Some
    function names have been changed to distinguish between RSA and El
    Gamal.

--- Assignment3.py
+++ Assignment3.py
@@ -1,10 +1,10 @@
 from random import randint
 
 '''def RSA_sign(text, d, n):
-	return (text^d) % n
+	return (text**d) % n
 
-def RSA_sign_verify(signature, e, d):
-	return (text^e) % n'''
+def RSA_sign_verify(signature, e, n):
+	return (signature**e) % n'''
 
 #Source for ExtEuclideanAlg and modInvEuclid from https://numericalrecipes.wordpress.com/tag/modular-multiplicative-inverse/
 def extEuclideanAlg(a, b) :
@@ -29,7 +29,7 @@ def modInvEuclid(a,m) :
         return None
 
 def find_multiplicative_inverse(euler_totient):
-	inverse1 = randint(2,euler_totient-1)
+	inverse1 = 54#randint(2,euler_totient-1)
 	inverse2 = None
 	while (inverse2 is None):
 		inverse1 = randint(2,euler_totient-1)
@@ -41,33 +41,68 @@ def choose_primes():
 	number2 = input("Please enter a second 4 digit prime: ")
 	return number1, number2
 
-def compute_keys(p, q):
+def RSA_compute_keys(p, q):
 	n = p*q
 	euler_totient = (p-1)*(q-1)
 	e, d = find_multiplicative_inverse(euler_totient)
 	return n, e, d
 
-def encrypt(key1, key2, plaintext):
+def ElGamal_compute_keys(p,g):
+	private_key = 43 #randint(1,100)
+	public_key = (g**private_key)%p
+	return private_key, public_key
+
+def RSA_encrypt(key1, key2, plaintext):
 	cipher = (plaintext**key2) % key1
 	print "encryption: ", plaintext, "^", key2, "mod", key1, "=", cipher
 	return cipher
 
-def decrypt(private, key1, cipher):
+def RSA_decrypt(private, key1, cipher):
 	msg = (cipher**private) % key1
 	print "decryption:", cipher, "^", private, "mod", key1, "=", msg
 	return msg
 
+def ElGamal_encrypt(plaintext, prime1, prime2, public_key):
+	k = 5#randint(1, prime1)
+	cipher = (plaintext * (public_key**k)) % prime1
+	u = (prime2 ** k) % prime1
+	print "u = g^k = ", prime2, "^", k, " = ", u, " mod ", prime1 
+	print "public key = ", public_key, "^", k, " = ", (public_key**k)%prime1 ," mod ", prime1
+	print "C = ", plaintext, "x", (public_key**k)%prime1, " = ", cipher, " mod ", prime1
+	return cipher, u, k
+
+def ElGamal_decrypt(ciphertext, u, private_key, public_key, k, prime1):
+	x = u**private_key
+	print "u^a = ", u, " ^ ", private_key, " = ", x%prime1, " mod p"
+	inverse1, inverse2 = find_multiplicative_inverse(prime1)
+	plaintext = (ciphertext * inverse2)%prime1
+	print "decryption: ", plaintext
+	return plaintext
+
 def rsa_main():
-	plaintext = input("Please enter a number: ")
+	plaintext = input("Please enter a number for your message: ")
 	prime1, prime2 = choose_primes()
-	public1, public2, private = compute_keys(prime1, prime2)
+	public1, public2, private = RSA_compute_keys(prime1, prime2)
 	print "PERSON ONE SENDS:", plaintext 
 	#signature = RSA_sign(plaintext, private, public1)
 	#print "SIGNATURE IS: ", signature
-	cipher = encrypt(public1, public2, plaintext)
+	cipher = RSA_encrypt(public1, public2, plaintext)
 	print "PERSON TWO RECIEVES: ", cipher
-	decrypted_msg = decrypt(private, public1, cipher)
+	decrypted_msg = RSA_decrypt(private, public1, cipher)
 	#verification = RSA_sign_verify(signature, public2, public1)
 	#print "SIGNATURE VERIFICATION: ", verification
 
-rsa_main()
+
+def gamal_main():
+	plaintext = input("Please enter a number for your message: ")
+	prime1, prime2 = choose_primes()
+	private_key, public_key = ElGamal_compute_keys(prime1, prime2)
+	print "PERSON ONE SENDS:", plaintext 
+	cipher, u, k = ElGamal_encrypt(plaintext, prime1, prime2, public_key)
+	print "PERSON TWO RECIEVES: ", cipher, "and", u
+	decrypted_msg = ElGamal_decrypt(cipher, u, private_key, public_key, k, prime1)
+
+
+
+#rsa_main()
+gamal_main()
